87903a5459d320a937dcf8fb9cf0c5c70dd83698
MATHIEU FONTAINE <mathieufontaine@mathieus-macbook-pro.home>
2020-10-30 18:23:45 +0100

    added svg animations

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,12 @@ inputs.forEach(input =>
 
 inputs.forEach(input => input.addEventListener("blur", validate));
 
+// Animations
+
+const computer = document.querySelector("svg #Computer");
+const picture = document.querySelector("svg #picture");
+const mobile = document.querySelector("svg #mobile");
+
 const startAnimation = () => {
   let tl = gsap.timeline({
     defaults: {
@@ -28,14 +34,37 @@ const startAnimation = () => {
     }
   });
 
+  const svgAnimation = () => {
+    tl.to(computer, 0.5, { opacity: 1, scale: 1.5, transformOrigin: "center" });
+    tl.to(computer, 0.5, { scale: 1 });
+    tl.to(picture, 0.5, { opacity: 1, scale: 1.5, transformOrigin: "center" });
+    tl.to(picture, 0.5, { scale: 1 });
+    tl.to(mobile, 0.5, { opacity: 1, scale: 1.5, transformOrigin: "center" });
+    tl.to(mobile, 0.5, { scale: 1 });
+  };
+
+  tl.to(computer, 0, { opacity: 0, scale: 0, transformOrigin: "center" });
+  tl.to(mobile, 0, { opacity: 0, scale: 0, transformOrigin: "center" });
+  tl.to(picture, 0, { opacity: 0, scale: 0, transformOrigin: "center" });
   tl.from(".showcase h2", { x: "-100vw" });
   tl.from(".showcase h3", { x: "100vw" }, "-=2");
   tl.from(".navbar", { x: "-100vw" }, "-=2");
-  tl.from([".showcase .headers", ".showcase img"], {
-    opacity: 0
-  });
-  tl.to(".nav-item", { x: 0, stagger: 0.1 }, "-=2");
+  tl.to(".nav-item", { x: 0, stagger: 0.2 }, "-=2");
+  tl.from(
+    [".showcase .headers", ".showcase svg"],
+    {
+      opacity: 0
+    },
+    "-=2"
+  );
+  svgAnimation();
+
+  setInterval(() => {
+    svgAnimation();
+  }, 10000);
 };
+
+startAnimation();
 
 const contactAnimation = () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -55,6 +84,3 @@ const contactAnimation = () => {
 
   tl.from(".form-container", { duration: 1, scale: 0 });
 };
-
-startAnimation();
-// contactAnimation();
