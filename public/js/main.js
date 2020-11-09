4ae4ead8d7ea49cb08168735be6bade4b1e479d6
MATHIEU FONTAINE <mathieufontaine@mathieus-macbook-pro.home>
2020-11-09 11:52:54 +0100

    navbar animations + page showcase style

--- public/js/main.js
+++ public/js/main.js
@@ -6,20 +6,36 @@
 
 const toggleMenuAnimation = () => {
   const hamburger = document.querySelector(".menu");
-  const hamburgerLines = document.querySelectorAll(".menu line");
+  const line1 = document.querySelectorAll(".menu .line1");
+  const line2 = document.querySelectorAll(".menu .line2");
+  const line3 = document.querySelectorAll(".menu .line3");
   const navOpen = document.querySelector(".nav-open");
   const logo = document.querySelector(".logo");
   const navItem = navOpen.querySelectorAll("li");
 
   const tl = gsap.timeline({ reversed: true });
 
-  tl.to(navOpen, 0.5, { y: 0 }).fromTo(
+  tl.to(navOpen, 0.5, { y: 0 }, 0);
+  tl.fromTo(
     navItem,
     0.5,
-    { opacity: 0, y: 50 },
+    { opacity: 0, y: 100 },
     { opacity: 1, y: 0, stagger: 0.1 },
-    "-=0.1"
+    "-=.5"
   );
+  tl.to(line1, 0.3, { rotation: 45, attr: { x2: 28, strokeWidth: 4 } }, "-=1");
+  tl.to(line2, 0, { opacity: 0 }, "-=1");
+  tl.to(
+    line3,
+    0.3,
+    {
+      transformOrigin: "left",
+      attr: { x2: 28, strokeWidth: 4 },
+      rotation: -45
+    },
+    "-=1"
+  );
+
   // .fromTo(logo, 0.2, { color: "white" }, { color: "black" }, "-=1")
   // .fromTo(
   //   hamburgerLines,
